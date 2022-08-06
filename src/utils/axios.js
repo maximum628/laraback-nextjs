e538a255805b12aac23f3d5a2e37fececc13b6dc
maxium628 <109892087+maxium628@users.noreply.github.com>
2022-08-06 20:48:30 +0800

    laraback-nextjs

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 // require('dotenv').config();
 
 // ----------------------------------------------------------------------
-console.log(process.env.BASE_SERVER_URL,555);
 const axiosInstance = axios.create({ baseURL: process.env.BASE_SERVER_URL || '' });
 
 axiosInstance.interceptors.response.use(
